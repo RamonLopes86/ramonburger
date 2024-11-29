09168b723fa07528ba598e289adcd5a4c75b81b3
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-29 01:11:56 -0300

    comecando o filter

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,106 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import React , {useState , useEffect} from "react";
+import array from "@/dados/dados";
+
+
+
+
+
+
 
 export default function Home() {
+
+
+
+  const [lista , setLista] = useState([])
+
+
+ 
+
+ 
+  
+  let filtro;
+
+  function exibirBebidas(param){
+   
+
+      if(param === 'bebida'){
+
+     filtro =  array.filter((itens)=>{
+   
+           return    itens.classe === 'bebida'
+     
+     
+         })
+     
+     
+         setLista(filtro)
+         
+   
+       }
+
+
+  
+ 
+
+    if(param === 'burger'){
+
+       filtro =  array.filter((itens)=>{
+
+        return    itens.classe === 'burger'
+  
+  
+      })
+  
+  
+      setLista(filtro)
+
+
+    }
+
+}
+
+
+useEffect(()=>{
+
+  exibirBebidas('burger')
+
+},[])
+
+
+ 
   return (
+
+
     <main>
 
-        ola , mundo
+      <button  onClick={()=>exibirBebidas('bebida')}>bebidas</button>
+      
+      <button onClick={()=>exibirBebidas('burger')}>burger</button>
 
+        {
+
+          lista.map((itens)=>{
+
+             return(
+
+              <div key={itens.id}>
+                   <Image alt="imagens de bebida" src={itens.img}/>
+                   <p>{itens.nome}</p>
+              </div>
+
+             )
+
+
+          })
+         
+
+        }
+     
+        
     </main>
   );
 }
---
+++ src/dados/dados.js
@@ -0,0 +1,206 @@
+import Image from "next/image"
+import burger1 from '../../public/burger1.jpg'
+
+import burger3 from '../../public/burger3.jpg'
+import burger4 from '../../public/burger4.jpg'
+import burger5 from '../../public/burger5.jpg'
+import burger6 from '../../public/burger6.jpg'
+import burger7 from '../../public/burger7.jpg'
+import burger8 from '../../public/burger8.jpg'
+import burger9 from '../../public/burger9.jpg'
+import bebida1 from '../../public/bebida1.jpg'
+import bebida2 from '../../public/bebida2.jpg'
+import bebida3 from '../../public/bebida3.jpg'
+import bebida4 from '../../public/bebida4.jpg'
+import bebida5 from '../../public/bebida5.jpg'
+import bebida6 from '../../public/bebida6.jpg'
+import bebida7 from '../../public/bebida7.jpg'
+import bebida8 from '../../public/bebida8.jpg'
+import bebida9 from '../../public/bebida9.jpg'
+import bebida10 from '../../public/bebida10.jpg'
+
+
+
+
+
+
+
+
+
+ const array = [
+
+
+    {
+        id:1,
+        img:burger1,
+        nome:'Gravato Travoli',
+        classe:'burger',
+        preco:40
+        
+
+    },
+
+
+    {
+        id:3,
+        img:burger3,
+        nome:'carvato xatavo',
+        classe:'burger',
+        preco:20
+        
+
+    },
+
+    {
+        id:4,
+        img:burger4,
+        nome:'chako x',
+        classe:'burger',
+        preco:75
+        
+
+    },
+
+    {
+        id:5,
+        img:burger5,
+        nome:'ratoli marato',
+        classe:'burger',
+        preco:90
+        
+
+    },
+
+    {
+        id:6,
+        img:burger6,
+        nome:'vitoko btto',
+        classe:'burger',
+        preco:25
+        
+
+    },
+    {
+        id:7,
+        img:burger7,
+        nome:'neyvo vaskko',
+        classe:'burger',
+        preco:18
+        
+    },
+    {
+        id:8,
+        img:burger8,
+        nome:'ninoko kravto',
+        classe:'burger',
+        preco:26
+        
+
+    },
+    {
+        id:9,
+        img:burger9,
+        nome:'niwitch lavg',
+        classe:'burger',
+        preco:15
+        
+
+    },
+
+    {
+        id:10,
+        img:bebida1,
+        nome:'vokoto resfresh',
+        classe:'bebida',
+        preco:12
+        
+
+    },
+    {
+        id:11,
+        img:bebida2,
+        nome:'limono frato',
+        classe:'bebida',
+        preco:17
+        
+
+    },
+    {
+        id:12,
+        img:bebida3,
+        nome:'cacato vasgra',
+        classe:'bebida',
+        preco:80
+        
+    },
+
+    {
+        id:13,
+        img:bebida4,
+        nome:'voksta pidioto',
+        classe:'bebida',
+        preco:16
+        
+
+    },
+    {
+        id:14,
+        img:bebida5,
+        nome:'galato ftas',
+        classe:'bebida',
+        preco:10
+        
+
+    },
+    {
+        id:15,
+        img:bebida6,
+        nome:'sixvato bf',
+        classe:'bebida',
+        preco:5
+        
+
+    },
+    {
+        id:16,
+        img:bebida7,
+        nome:'seven pecads',
+        classe:'bebida',
+        preco:21
+        
+
+    },
+    {
+        id:17,
+        img:bebida8,
+        nome:'eit nuvix',
+        classe:'bebida',
+        preco:18
+        
+
+    },
+    {
+        id:18,
+        img:bebida9,
+        nome:'warfrash',
+        classe:'bebida',
+        preco:40
+        
+
+    },
+    {
+        id:19,
+        img:bebida10,
+        nome:'tramonster evolit',
+        classe:'bebida',
+        preco:19
+        
+
+    },
+
+
+]
+
+
+
+export default array
