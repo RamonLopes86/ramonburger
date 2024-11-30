558535fb36ae480b40e86f934581824311bfac10
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-11-30 02:16:18 -0300

    ajuste de section banner

--- src/components/sectionBanner/banner.jsx
+++ src/components/sectionBanner/banner.jsx
@@ -2,6 +2,8 @@ import estiloBanner from './banner.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import hamburger from '../../../public/burgerBanner.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone , faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Apresentacao(){
@@ -13,14 +15,14 @@ export default function Apresentacao(){
 
                     <section className={estiloBanner.boxTexto}>
 
-                        <h1>Escolha sua comida favorita</h1>
+                        <h1>Escolha sua comida <strong>favorita</strong></h1>
                         <p>Aproveite nosso cardápio ! Escolha o que desejar e receba em sua casa de forma rápida e segura</p>  
 
 
                         <div className={estiloBanner.boxBtn}>
 
                                 <button>Ver cardápio</button>
-                                <Link className={estiloBanner.link} href={''}><button>(71)99126-5530</button></Link>
+                                <Link className={estiloBanner.link} href={''}><button><FontAwesomeIcon className={estiloBanner.iconPhone} icon={faPhone}/> 7199126-5530</button></Link>
                           
                         </div>      
                            
@@ -37,7 +39,7 @@ export default function Apresentacao(){
 
                                 <div className={estiloBanner.msg}>
 
-                                    <p>"Entrega rápida e funcionários simpáticos. A comida chegou quente e muito saborosa"</p>
+                                    <p> <FontAwesomeIcon className={estiloBanner.aspas} icon={faQuoteLeft}/> Entrega rápida e funcionários simpáticos. A comida chegou quente e muito saborosa" <FontAwesomeIcon className={estiloBanner.aspas} icon={faQuoteRight}/> </p>
                                     <span>Ramon Lopes</span>
 
                                 </div>
